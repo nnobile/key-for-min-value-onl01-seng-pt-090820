edae2234e49d1a64893a3e5676124b687c7536ac
Nick Nobile <nickcnobile@gmail.com>
2020-09-20 13:11:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,6 @@
 #Build a method key_for_min_value that accepts an argument of a hash.
 #This method should iterate over the hash and return the key (not the value!) that points to the smallest value of the set.
 #If the method is called and passed an argument of an empty hash, it should return nil.
-weather = { :monday => 55, :tuesday => 45, :wednesday => 65}
-
 def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
